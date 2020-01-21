8d3dc986a77baae838ebb4bb5a8817c479a046f4
TakuyaHanada <takuyahanada.hntk@gmail.com>
2020-01-21 20:07:53 +0900

    [add] Logoの作成

---
+++ src/components/atoms/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  display: inline-block;
+  margin: 0;
+  font-size: 2.4rem;
+`
+
+export default function Logo () {
+  return (
+    <H1>ohakutsu</H1>
+  )
+}
