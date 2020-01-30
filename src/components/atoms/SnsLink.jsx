e7f8b4e3660832c4dd0b8ae743fad973c1347072
TakuyaHanada <takuyahanada.hntk@gmail.com>
2020-01-30 23:25:01 +0900

    [update] SnsLinkに変更、修正

---
+++ src/components/atoms/SnsLink.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const Button = styled.a`
+  display: inline-block;
+  margin: 5px;
+  padding: 5px 10px;
+  border: 1px solid #3f51b5;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  color: #3f51b5;
+  text-decoration: none;
+  transition: background-color 0.2s, color 0.2s;
+  :hover {
+    background-color: #3f51b5;
+    color: #ffffff;
+  }
+`
+
+export default function SnsLink ({ href, text }) {
+  return (
+    <Button href={href}>{text}</Button>
+  )
+}
+
+SnsLink.propTypes = {
+  href: PropTypes.string,
+  text: PropTypes.string
+}
