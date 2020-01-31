0fb2204434c97a201aadd826a08e34c075fad31c
TakuyaHanada <takuyahanada.hntk@gmail.com>
2020-01-31 11:45:30 +0900

    [update] getPostsDateメソッドの作成とその他の修正

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,27 +1,27 @@
 import axios from 'axios'
 
-const apiUrl = 'https://us-central1-ohakutsuportfolio.cloudfunctions.net'
-
 export default class Api {
   static async getProfileData () {
+    const apiUrl = 'https://us-central1-ohakutsuportfolio.cloudfunctions.net'
+
     const profilePromise = axios.get(`${apiUrl}/profile`)
     const linksPromise = axios.get(`${apiUrl}/links`)
 
     const profileResponse = await profilePromise
     const linksResponse = await linksPromise
 
-    const profileData = profileResponse.data
-    const linksData = linksResponse.data
+    let imageSrc, description, snsLinks
 
-    let imageSrc, description
-    let snsLinks = []
+    if (profileResponse.status === 200) {
+      const profileData = profileResponse.data
 
-    if (profileData.status_code === 200) {
       const { imageUrl, desc } = profileData.data.profile
       imageSrc = imageUrl
       description = desc
     }
-    if (linksData.status_code === 200) {
+    if (linksResponse.status === 200) {
+      const linksData = linksResponse.data
+
       const { links } = linksData.data
       snsLinks = links
     }
@@ -29,7 +29,26 @@ export default class Api {
     return { imageSrc, description, snsLinks }
   }
 
-  static getPostsData () {
+  static async getPostsData () {
+    const apiUrl = 'https://qiita.com/api/v2/items?page=1&per_page=5&query=qiita+user%3Aohakutsu'
 
+    const response = await axios.get(apiUrl)
+
+    let posts
+
+    if (response.status === 200) {
+      const data = response.data
+
+      posts = data.map(d => {
+        const date = d.created_at
+        const title = d.title
+        const provider = 'Qiita'
+        const link = d.url
+
+        return { date, title, provider, link }
+      })
+    }
+
+    return { posts }
   }
 }
