d5c3687206e2dd7c2f20a9b5adeec0ba36facb35
TakuyaHanada <takuyahanada.hntk@gmail.com>
2020-01-31 11:25:28 +0900

    [update] linksのデータも取得

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,15 +4,29 @@ const apiUrl = 'https://us-central1-ohakutsuportfolio.cloudfunctions.net'
 
 export default class Api {
   static async getProfileData () {
-    const response = await axios.get(`${apiUrl}/profile`)
-    const data = response.data
+    const profilePromise = axios.get(`${apiUrl}/profile`)
+    const linksPromise = axios.get(`${apiUrl}/links`)
 
-    if (data.status_code === 200) {
-      const { imageUrl: imageSrc, desc: description } = data.data.profile
-      return { imageSrc, description }
-    } else {
-      return { imageSrc: undefined, description: undefined }
+    const profileResponse = await profilePromise
+    const linksResponse = await linksPromise
+
+    const profileData = profileResponse.data
+    const linksData = linksResponse.data
+
+    let imageSrc, description
+    let snsLinks = []
+
+    if (profileData.status_code === 200) {
+      const { imageUrl, desc } = profileData.data.profile
+      imageSrc = imageUrl
+      description = desc
     }
+    if (linksData.status_code === 200) {
+      const { links } = linksData.data
+      snsLinks = links
+    }
+
+    return { imageSrc, description, snsLinks }
   }
 
   static getPostsData () {
